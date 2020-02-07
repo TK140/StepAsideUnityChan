4189b0d632249b639a078178064c3fc21eb25569
TK140 <yukimitsu.shimakawa@qdenbs.com>
2020-02-07 15:17:14 +0900

    Lesson6の課題です。
    確認お願いいたします。

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+ {
+    //Unityちゃんのオブジェクト	
+    private GameObject unitychan;
+    //UnityちゃんのＺ軸	
+    private float unityichi;
+
+    // Use this for initialization	
+    void Start()
+    {
+
+        //Unityちゃんのオブジェクトを取得	
+        unitychan = GameObject.Find("unitychan");
+
+    }
+
+    // Update is called once per frame	
+    void Update()
+    {
+
+        //Unityちゃんの座標Ｚを取得	
+        unityichi = unitychan.transform.position.z;
+
+        if (unityichi - 3 > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
+
